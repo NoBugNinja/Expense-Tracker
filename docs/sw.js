2327080405c9ce047cab93cea767ac0de74946c8
Mohammed shafiq <222201854+NoBugNinja@users.noreply.github.com>
2025-09-02 23:55:24 +0530

    sw change4

--- docs/sw.js
+++ docs/sw.js
@@ -1,40 +1,45 @@
-const CACHE_NAME = 'expense-tracker-v3';
+const CACHE_NAME = 'expense-tracker-v4';
+
+// Find the base path dynamically from where sw.js is located
+const BASE_PATH = self.location.pathname.replace(/sw\.js$/, '');
+
 const ASSETS_TO_CACHE = [
-  'index.html',
-  'script.js',
-  'output.css',
-  'images/icon-192x192.png',
-  'images/icon-512x512.png'
+  BASE_PATH,
+  BASE_PATH + 'index.html',
+  BASE_PATH + 'script.js',
+  BASE_PATH + 'output.css',
+  BASE_PATH + 'images/icon-192x192.png',
+  BASE_PATH + 'images/icon-512x512.png'
 ];
 
-// Install event
+// Install
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      console.log('Opened cache and caching assets');
+      console.log('Opened cache and caching assets:', ASSETS_TO_CACHE);
       return cache.addAll(ASSETS_TO_CACHE);
     })
   );
   self.skipWaiting();
 });
 
-// Activate event
+// Activate
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
+    caches.keys().then(cacheNames =>
+      Promise.all(
         cacheNames.map(cache => {
           if (cache !== CACHE_NAME) {
             console.log('Service Worker: clearing old cache');
             return caches.delete(cache);
           }
         })
-      );
-    })
+      )
+    )
   );
 });
 
-// Fetch event
+// Fetch
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
